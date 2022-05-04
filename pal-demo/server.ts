c7bc939f7d1e1718312a4f4a2674a7ccdb71350c
Edward Nuno <edwardpax@google.com>
2022-05-04 11:30:00 -0700

    Add new pal-specific demo dir
    Add new pal-specific demo folder based off of existing demo folder and
    change nodemon config to point to pal-demo

---
+++ pal-demo/server.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+import payload from '../src';
+
+const expressApp = express();
+
+expand(dotenv.config());
+
+expressApp.use('/static', express.static(path.resolve(__dirname, 'client/static')));
+
+payload.init({
+  secret: 'SECRET_KEY',
+  mongoURL: process.env.DB_URL,
+  express: expressApp,
+  email: {
+    fromName: 'Payload',
+    fromAddress: 'hello@payloadcms.com',
+  },
+  onInit: (app) => {
+    app.logger.info('Payload Demo Initialized');
+  },
+});
+
+const externalRouter = express.Router();
+
+externalRouter.use(payload.authenticate);
+
+externalRouter.get('/', (req, res) => {
+  if (req.user) {
+    return res.send(`Authenticated successfully as ${req.user.email}.`);
+  }
+
+  return res.send('Not authenticated');
+});
+
+expressApp.use('/external-route', externalRouter);
+
+expressApp.listen(3000, async () => {
+  payload.logger.info(`Admin URL on ${payload.getAdminURL()}`);
+  payload.logger.info(`API URL on ${payload.getAPIURL()}`);
+});
